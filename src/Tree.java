b4f3baea97e5796fedf392913db25cc06afbd802
K <kaixiang.xu@mail.utoronto.ca>
2023-09-19 00:42:45 -0400

    TreeMultiSet added all methods

--- src/Tree.java
+++ src/Tree.java
@@ -1,2 +1,35 @@
 public class Tree {
 }
+
+public class TreeMultiSet extends Multiset{
+    private Tree tree;
+
+    public TreeMultiSet(){
+        this.tree = new Tree();
+    }
+
+    public Boolean add(Object item){
+        tree.insert(item);
+        return true;
+    }
+
+    public void remove(Object item){
+        tree.delete_item(item);
+    }
+
+    public Boolean contains(Object item){
+        return tree.contains(item);
+    }
+
+    public Boolean isEmpty(){
+        return tree.isEmpty();
+    }
+
+    public Integer count(Object item){
+        return tree.count(item);
+    }
+
+    public Integer size(){
+        return tree.size();
+    }
+}
